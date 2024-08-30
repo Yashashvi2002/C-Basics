30d120e1bbc6518fe880f08ec1ba939b7680ad16
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-30 17:22:13 +0530

    Add project files.

---
+++ C# Basics And OOPS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Basics_And_OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ OPPS_Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OPPS_Basics
+{
+  //Encapsulation Example
+  //Example 1
+   public class Employee
+    {
+        private string name;
+        private int empId;
+        private int salary;
+        public Employee(string name, int empId, int salary)
+        {
+            this.name = name;
+            this.empId = empId;
+            this.salary = salary;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("EmpId: " + empId);
+            Console.WriteLine("Salary: " + salary);
+        }
+    }
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("John", 101, 50000);
+            emp.Display();
+        }
+        
+    }
+}
