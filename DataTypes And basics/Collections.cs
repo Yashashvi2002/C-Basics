7e6d4d2fd0b965e85f9b6077a42901f22a269e88
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-09-05 12:29:37 +0530

    Collections and its types

--- DataTypes And basics/Collections.cs
+++ DataTypes And basics/Collections.cs
@@ -1,4 +1,5 @@
-//Generics
+//Collections are of two types Generic and Non-generic
+//Non-generic collections are not type safe and can store any type of data
 using System;
 
 namespace Generics
@@ -108,6 +109,86 @@ namespace Generics
 
             MyGenericClass<double> doubleGenericClass = new MyGenericClass<double>(10.5);
             doubleGenericClass.GenericMethod(20.5);
+        }
+    }
+}
+
+
+namespace NonGeneric
+{
+    class DataStore
+    {
+        public object[] data = new object[4];
+
+        // Method to add or update the data at the specified index
+        public void AddOrUpdate(int index, object item)
+        {
+            if (index >= 0 && index < 4)
+            {
+                data[index] = item;
+            }
+        }
+        // Method to get the data at the specified index
+        public object GetData(int index)
+        {
+            if (index >= 0 && index < 10)
+            {
+                return data[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // Method to print the entire array
+        public void PrintData()
+        {
+            Console.Write("\nArray = [");
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write($"{data[i]}  ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+
+
+    class KeyValuePair
+    {
+        public object Key { get; set; }
+        public object Value { get; set; }
+
+        public void GenericMethod()
+        {
+            Console.WriteLine("\nKey type: " + Key.GetType() + ", value: " + Key);
+            Console.WriteLine("Value type: " + Value.GetType() + ", value: " + Value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new instance of the non-generic class
+            DataStore cities = new DataStore();
+            cities.AddOrUpdate(0, "Mumbai");
+            cities.AddOrUpdate(1, "Chicago");
+            cities.AddOrUpdate(2, 1123);
+            cities.AddOrUpdate(3, 10.5);
+            string city = (string)cities.GetData(0);
+            Console.WriteLine("City: " + city);
+            cities.PrintData();
+
+            KeyValuePair kvp1 = new KeyValuePair();
+            kvp1.Key = 100;
+            kvp1.Value = "Hundred";
+            kvp1.GenericMethod();
+
+            KeyValuePair kvp2 = new KeyValuePair();
+            kvp2.Key = "India";
+            kvp2.Value = "New Delhi";
+            kvp2.GenericMethod();
         }
     }
 }
