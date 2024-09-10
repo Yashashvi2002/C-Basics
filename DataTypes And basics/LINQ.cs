794d23dc485b88a46d6cc83e4988cdc88382300a
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-09-10 17:56:51 +0530

    Finding Even and Odd using LINQ query and LINQ method

--- DataTypes And basics/LINQ.cs
+++ DataTypes And basics/LINQ.cs
@@ -87,6 +87,31 @@ class Program
         }
 
 
+
+        //Finiding even and odd numbers
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // LINQ Query: Even numbers
+        var evenNumbers = from num in numbers
+                          where num % 2 == 0
+                          select num;
+
+        Console.WriteLine("Even numbers:");
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        // LINQ Method : Odd numbers
+        var oddNumbers = numbers.Where(num => num % 2 != 0);
+
+        Console.WriteLine("Odd numbers:");
+        foreach (var num in oddNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
     }
 }
 
--- OPPS_Basics/arrayAndSortingAlgorithms.cs
+++ OPPS_Basics/arrayAndSortingAlgorithms.cs
@@ -124,7 +124,7 @@ namespace SortingAlgorithms
         {
             for (int i = 0; i < numbers.Length; i++)
             {
-                for (int j = 0; j < numbers.Length - 1; j++)
+                for (int j = 0; j < numbers.Length -i- 1; j++)
                 {
                     if (numbers[j] > numbers[j + 1])
                     {
